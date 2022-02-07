bd7b4b03d15bee05d47f667dce9484acc00e0dd9
HHJ121 <dandylioneffect@gmail.com>
2022-02-06 21:48:34 -0600

    Added API integration

--- src/App.js
+++ src/App.js
@@ -2,24 +2,17 @@ import React from "react";
 
 import './App.css';
 import Weather from "./Weather";
-import Typewriter from "typewriter-effect";
+
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <div className="App-background">
-      <header className="App-header">
-        <Typewriter 
-        options={{
-          strings: ["Hello.", "Welcome to my weather app.", "Where would you like to look up the weather?"],
-          autoStart: true,
-          loop: true,
-          pauseFor: 1800,
-        }} 
-        />
+      <main className="App-main">
+        
         <Weather />
-      </header>
+      </main>
       </div>
       <footer className="links">
         <a href="https://github.com/HHJ121/antique-weather-application" target="_blank" rel="noreferrer">
--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import Typewriter from "typewriter-effect";
 
 import "./Weather.css";
 
 export default function Weather() {
+  const [city, setCity] = useState("");
+  const [searchloaded, setSearchLoaded] = useState(false);
+  const [weatherOverview, setWeatherOverview] = useState("");
+
+  function displayWeatherOverview(response) {
+    setSearchLoaded(true);
+  }
+
+  function search(city) {
+    const apiKey = "c0d5182ce71bc2be9c80f43da3c8ee07";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(displayWeatherOverview);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function updateCity(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
+
+  if (searchloaded) {
     return (
-        <div className="Weather">
-            <form className="search-form">
-                <input type="search" placeholder="Enter a city" autoComplete="no" autoFocus="on" />
-                <input type="submit" value="🔍" />
-            </form>
-        </div>
+      <div className="Weather">
+        <Typewriter
+          options={{
+            strings: [
+              "Hello.",
+              "Welcome to my weather app.",
+              "Where would you like to look up the weather?",
+            ],
+            autoStart: true,
+            loop: true,
+            pauseFor: 1800,
+          }}
+        />
+        <form className="search-form">
+          <input
+            type="search"
+            placeholder="Enter a city"
+            autoComplete="no"
+            autoFocus="on"
+            onChange={updateCity}
+          />
+          <input type="submit" value="🔍" onSubmit={handleSubmit} />
+        </form>
+      </div>
     );
+  } else {
+    search();
+    return (
+      <div className="Weather">
+        <Typewriter
+          options={{
+            strings: [
+              "Loading . . . ",
+              "Just gathering all the data for you . . .",
+              "Thank you for your patience.",
+            ],
+            autoStart: true,
+            loop: true,
+            pauseFor: 3000,
+          }}
+        />
+        <form className="search-form">
+          <input
+            type="search"
+            placeholder="Enter a city"
+            autoComplete="no"
+            autoFocus="on"
+            onChange={updateCity}
+          />
+          <input type="submit" value="🔍" onSubmit={handleSubmit} />
+        </form>
+      </div>
+    );
+  }
 }
