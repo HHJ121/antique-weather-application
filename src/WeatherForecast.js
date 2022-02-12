2f189cfb4db4f2faea4baeb25d7c6455c49d9b43
HHJ121 <dandylioneffect@gmail.com>
2022-02-11 23:06:51 -0600

    Updated by refactoring the component

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -4,53 +4,24 @@ import axios from "axios";
 
 export default function WeatherForecast(props) {
   let [forecastLoaded, setForecastLoaded] = useState(false);
-  let [forecastData, setForecastData] = useState(null);
+  let [forecastData, setForecastData] = useState(" ");
 
   useEffect(() => {
-      setForecastLoaded(false);
+    setForecastLoaded(false);
   }, [props.coordinates]);
 
-  function displayMaxTemp() {
-    let maxTemp = forecastData.temp.max;
-
-    return `${maxTemp}`;
-  }
-
-  function displayMinTemp() {
-    let minTemp = forecastData.temp.min;
-
-    return `${minTemp}`;
-  }
-
-  function displayForecastDay(time) {
-    let forecastDay = new Date(forecastData.dt * 1000);
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-
-    let day = days[forecastDay.getDay()];
-
-    return `${day}`;
-  }
-
-  function showWeatherForecastData(response) {
-    setForecastData(response.data.daily);
-    setForecastLoaded(true);
-  }
-
-  function handleForecastLoad() {
+  function loadForecast() {
     let apiKey = "c0d5182ce71bc2be9c80f43da3c8ee07";
     let lon = props.coordinates.lon;
     let lat = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(showWeatherForecastData);
+  }
+
+  function showWeatherForecastData(response) {
+    setForecastData(response.data.daily);
+    setForecastLoaded(true);
   }
 
   if (forecastLoaded) {
@@ -61,7 +32,24 @@ export default function WeatherForecast(props) {
             if (index > 0 && index < 5) {
               return (
                 <div key={index}>
-                  <div className="col forecast-day">{displayForecastDay()}</div>
+                  <div className="col forecast-day">
+                    {() => {
+                      let forecastDay = new Date(dailyForecast.dt * 1000);
+                      let days = [
+                        "Sunday",
+                        "Monday",
+                        "Tuesday",
+                        "Wednesday",
+                        "Thursday",
+                        "Friday",
+                        "Saturday",
+                      ];
+
+                      let day = days[forecastDay.getDay()];
+
+                      return `${day}`;
+                    }}
+                  </div>
                   <div className="col forecast-weather-icon">
                     <AnimatedWeatherIcon
                       iconCode={dailyForecast.weather[0].icon}
@@ -70,8 +58,13 @@ export default function WeatherForecast(props) {
                   </div>
 
                   <div className="col forecast-temp">
-                    <span className="max-temp">{displayMaxTemp()}°</span> |{" "}
-                    <span className="min-temp">{displayMinTemp()}°</span>
+                    <span className="max-temp">
+                      {Math.round(dailyForecast.temp.max)}°
+                    </span>{" "}
+                    |{" "}
+                    <span className="min-temp">
+                      {Math.round(dailyForecast.temp.min)}°
+                    </span>
                   </div>
                 </div>
               );
@@ -83,7 +76,7 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    handleForecastLoad();
+    loadForecast();
 
     return null;
   }
